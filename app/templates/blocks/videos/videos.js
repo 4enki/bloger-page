6e626c5bf272237ed9d737a80907e6e37a15e7b9
Sergey Glazov <yaglazov@gmail.com>
2018-03-12 12:44:31 +0300

    скрипт для табов в видео

--- app/templates/blocks/videos/videos.js
+++ app/templates/blocks/videos/videos.js
@@ -2,3 +2,14 @@ $('.youtube-link').magnificPopup({
   type: 'iframe',
   preloader: false
 });
+
+$(document).ready(function() {
+  $(".work-types a.work-type").click(function(event) {
+    event.preventDefault();
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+    var tab = $(this).attr("href");
+    $(".works-example").not(tab).css("display", "none");
+    $(tab).fadeIn();
+  });
+});
